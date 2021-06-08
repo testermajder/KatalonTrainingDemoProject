0f9366eb45b27ea517fc2e8384c29e9b633755f0
Mila Ajder <majder@execom.eu>
2021-06-08 14:13:57 +0200

    My first project commit (Mila).

---
+++ Test Suites/TestSuiteDemo1.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>023dc3c1-1919-40a4-a294-012ccdefa6da</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ccec892-1998-48e2-b5bd-974ba9902466</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Demo/ManualModeDemo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2a869107-017d-46b8-9399-1f95bf197740</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>81babaf0-a66b-4a4e-a491-6b32747ae704</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/ScriptModeDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
